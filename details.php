e5f249f91eddbc785c5ad67b4c1b59347ae61806
Parthadeb Mondal <parthadev76@gmail.com>
2020-11-04 02:00:29 +0600

    templating done

--- details.php
+++ details.php
@@ -24,7 +24,7 @@
   <div class="wrap">
 		<div class="header_top">
 			<div class="logo">
-				<a href="index.html"><img src="images/logo.png" alt="" /></a>
+				<a href="index.php"><img src="images/logo.png" alt="" /></a>
 			</div>
 			  <div class="header_top_right">
 			    <div class="search_box">
@@ -40,17 +40,19 @@
 							</a>
 						</div>
 			      </div>
-		   <div class="login"><a href="login.html">Login</a></div>
+		   <div class="login">
+		   	   <span><a href="login.html">Login</a></span>
+		   </div>
 		 <div class="clear"></div>
 	 </div>
 	 <div class="clear"></div>
  </div>
 <div class="menu">
 	<ul id="dc_mega-menu-orange" class="dc_mm-orange">
-	  <li><a href="index.html">Home</a></li>
+	  <li><a href="index.php">Home</a></li>
 	  <li><a href="products.html">Products</a> </li>
 	  <li><a href="topbrands.html">Top Brands</a></li>
-	  <li><a href="cart.html">Cart</a></li>
+	   <li><a href="cart.html">Cart</a></li>
 	  <li><a href="contact.html">Contact</a> </li>
 	  <div class="clear"></div>
 	</ul>
@@ -58,75 +60,54 @@
 
  <div class="main">
     <div class="content">
-    	<div class="content_top">
-    		<div class="heading">
-    		<h3>Latest from Iphone</h3>
-    		</div>
-    		<div class="clear"></div>
-    	</div>
-	      <div class="section group">
-				<div class="grid_1_of_4 images_1_of_4">
-					 <a href="preview-3.html"><img src="images/feature-pic1.png" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-					 <p><span class="price">$505.22</span></p>
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-					<a href="preview-2.html"><img src="images/feature-pic2.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-					 <p><span class="price">$620.87</span></p> 
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-					<a href="preview-4.html"><img src="images/feature-pic3.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-					 <p><span class="price">$220.97</span></p>
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-					<img src="images/feature-pic4.png" alt="" />
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-					 <p><span class="price">$415.54</span></p> 
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-				
-				
-				<div class="grid_1_of_4 images_1_of_4" style="margin-left:0">
-					 <a href="preview-3.html"><img src="images/new-pic1.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p><span class="price">$403.66</span></p>
-				    
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-					<a href="preview-4.html"><img src="images/new-pic2.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p><span class="price">$621.75</span></p>
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-					<a href="preview-2.html"><img src="images/feature-pic2.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p><span class="price">$428.02</span></p>
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-				 <img src="images/new-pic3.jpg" alt="" />
-					 <h2>Lorem Ipsum is simply </h2>					 
-					 <p><span class="price">$457.88</span></p>   
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
+    	<div class="section group">
+				<div class="cont-desc span_1_of_2">				
+					<div class="grid images_3_of_2">
+						<img src="images/preview-img.jpg" alt="" />
+					</div>
+				<div class="desc span_3_of_2">
+					<h2>Lorem Ipsum is simply dummy text </h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>					
+					<div class="price">
+						<p>Price: <span>$500</span></p>
+						<p>Category: <span>Laptop</span></p>
+						<p>Brand:<span>Samsnumg</span></p>
+					</div>
+				<div class="add-cart">
+					<form action="cart.html" method="post">
+						<input type="number" class="buyfield" name="" value="1"/>
+						<input type="submit" class="buysubmit" name="submit" value="Buy Now"/>
+					</form>				
 				</div>
 			</div>
-
-	
-	
-    </div>
- </div>
-</div>
+			<div class="product-desc">
+			<h2>Product Details</h2>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+	        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+	    </div>
+				
+	</div>
+				<div class="rightsidebar span_3_of_1">
+					<h2>CATEGORIES</h2>
+					<ul>
+				      <li><a href="productbycat.html">Mobile Phones</a></li>
+				      <li><a href="productbycat.html">Desktop</a></li>
+				      <li><a href="productbycat.html">Laptop</a></li>
+				      <li><a href="productbycat.html">Accessories</a></li>
+				      <li><a href="productbycat.html#">Software</a></li>
+					   <li><a href="productbycat.html">Sports & Fitness</a></li>
+					   <li><a href="productbycat.html">Footwear</a></li>
+					   <li><a href="productbycat.html">Jewellery</a></li>
+					   <li><a href="productbycat.html">Clothing</a></li>
+					   <li><a href="productbycat.html">Home Decor & Kitchen</a></li>
+					   <li><a href="productbycat.html">Beauty & Healthcare</a></li>
+					   <li><a href="productbycat.html">Toys, Kids & Babies</a></li>
+    				</ul>
+    	
+ 				</div>
+ 		</div>
+ 	</div>
+	</div>
    <div class="footer">
    	  <div class="wrapper">	
 	     <div class="section group">
@@ -154,7 +135,7 @@
 					<h4>My account</h4>
 						<ul>
 							<li><a href="contact.html">Sign In</a></li>
-							<li><a href="index.html">View Cart</a></li>
+							<li><a href="index.php">View Cart</a></li>
 							<li><a href="#">My Wishlist</a></li>
 							<li><a href="#">Track My Order</a></li>
 							<li><a href="faq.html">Help</a></li>
@@ -179,9 +160,9 @@
 				</div>
 			</div>
 			<div class="copy_right">
-				<p>Compant Name © All rights Reseverd </p>
+				<p>Compant Name © All rights Reseverd</a> </p>
 		   </div>
-     </div>
+     </div>  
     </div>
     <script type="text/javascript">
 		$(document).ready(function() {
--- productbycat.php
+++ productbycat.php
@@ -24,7 +24,7 @@
   <div class="wrap">
 		<div class="header_top">
 			<div class="logo">
-				<a href="index.html"><img src="images/logo.png" alt="" /></a>
+				<a href="index.php"><img src="images/logo.png" alt="" /></a>
 			</div>
 			  <div class="header_top_right">
 			    <div class="search_box">
@@ -36,7 +36,7 @@
 					<div class="cart">
 						<a href="#" title="View my shopping cart" rel="nofollow">
 								<span class="cart_title">Cart</span>
-									<span class="no_product">(empty)</span>
+								<span class="no_product">(empty)</span>
 							</a>
 						</div>
 			      </div>
@@ -47,7 +47,7 @@
  </div>
 <div class="menu">
 	<ul id="dc_mega-menu-orange" class="dc_mm-orange">
-	  <li><a href="index.html">Home</a></li>
+	  <li><a href="index.php">Home</a></li>
 	  <li><a href="products.html">Products</a> </li>
 	  <li><a href="topbrands.html">Top Brands</a></li>
 	  <li><a href="cart.html">Cart</a></li>
@@ -60,7 +60,7 @@
     <div class="content">
     	<div class="content_top">
     		<div class="heading">
-    		<h3>Acer</h3>
+    		<h3>Latest from Iphone</h3>
     		</div>
     		<div class="clear"></div>
     	</div>
@@ -93,15 +93,9 @@
 					 <p><span class="price">$415.54</span></p> 
 				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
 				</div>
-			</div>
-		<div class="content_bottom">
-    		<div class="heading">
-    		<h3>Samsung</h3>
-    		</div>
-    		<div class="clear"></div>
-    	</div>
-			<div class="section group">
-				<div class="grid_1_of_4 images_1_of_4">
+				
+				
+				<div class="grid_1_of_4 images_1_of_4" style="margin-left:0">
 					 <a href="preview-3.html"><img src="images/new-pic1.jpg" alt="" /></a>
 					 <h2>Lorem Ipsum is simply </h2>
 					 <p><span class="price">$403.66</span></p>
@@ -127,39 +121,9 @@
 				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
 				</div>
 			</div>
-	<div class="content_bottom">
-    		<div class="heading">
-    		<h3>Canon</h3>
-    		</div>
-    		<div class="clear"></div>
-    	</div>
-			<div class="section group">
-				<div class="grid_1_of_4 images_1_of_4">
-					 <a href="preview-3.html"><img src="images/new-pic1.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p><span class="price">$403.66</span></p>
-				    
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-					<a href="preview-4.html"><img src="images/new-pic2.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p><span class="price">$621.75</span></p>
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-					<a href="preview-2.html"><img src="images/feature-pic2.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p><span class="price">$428.02</span></p>
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-				 <img src="images/new-pic3.jpg" alt="" />
-					 <h2>Lorem Ipsum is simply </h2>					 
-					 <p><span class="price">$457.88</span></p>   
-				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
-				</div>
-			</div>
+
+	
+	
     </div>
  </div>
 </div>
@@ -190,7 +154,7 @@
 					<h4>My account</h4>
 						<ul>
 							<li><a href="contact.html">Sign In</a></li>
-							<li><a href="index.html">View Cart</a></li>
+							<li><a href="index.php">View Cart</a></li>
 							<li><a href="#">My Wishlist</a></li>
 							<li><a href="#">Track My Order</a></li>
 							<li><a href="faq.html">Help</a></li>
